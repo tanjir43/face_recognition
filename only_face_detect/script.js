93e6095fed6edd13ae9fdc6f3873633a4ca7a0de
Tanjir43 <tanjirislam7@gmail.com>
2022-10-25 17:44:25 +0600

    only image

---
+++ only_face_detect/script.js
@@ -0,0 +1,36 @@
+var video = document.querySelector('video');
+var constraints = { video: { width: 640, height: 480  } };
+
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+
+    Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(video.play())
+  // video.play();
+};
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); })
+;
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
+
